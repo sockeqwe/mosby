ecda070719d85821d36378ad1a050c0c76b3ad01
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2015-04-13 12:08:16 +0200

    Drwaer / Arrow color

--- sample-mail/src/main/java/com/hannesdorfmann/mosby/sample/mail/details/DetailsActivity.java
+++ sample-mail/src/main/java/com/hannesdorfmann/mosby/sample/mail/details/DetailsActivity.java
@@ -1,5 +1,7 @@
 package com.hannesdorfmann.mosby.sample.mail.details;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,10 +24,14 @@ public class DetailsActivity extends MosbyActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_mail_details);
 
-    toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+    toolbar.setNavigationIcon(getBackArrowDrawable());
     toolbar.setNavigationOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
-
+        if (Build.VERSION.SDK_INT >= 21) {
+          finishAfterTransition();
+        } else {
+          finish();
+        }
       }
     });
 
@@ -40,6 +46,15 @@ public class DetailsActivity extends MosbyActivity {
       getSupportFragmentManager().beginTransaction()
           .replace(R.id.fragmentContainer, fragment)
           .commit();
+    }
+  }
+
+  private Drawable getBackArrowDrawable() {
+
+    if (Build.VERSION.SDK_INT >= 21) {
+      return getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha, getTheme());
+    } else {
+      return getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
     }
   }
 }

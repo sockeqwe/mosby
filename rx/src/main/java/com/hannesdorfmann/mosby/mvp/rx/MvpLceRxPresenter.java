bbfdff7c9492cfb23728e60ec6fe31d47ef4eedf
Fahim Karim <fahim@jet.com>
2015-04-21 14:54:11 -0400

    missed two files

--- rx/src/main/java/com/hannesdorfmann/mosby/mvp/rx/MvpLceRxPresenter.java
+++ rx/src/main/java/com/hannesdorfmann/mosby/mvp/rx/MvpLceRxPresenter.java
@@ -17,7 +17,7 @@
 package com.hannesdorfmann.mosby.mvp.rx;
 
 import com.hannesdorfmann.mosby.mvp.lce.MvpLceView;
-import com.hannesdorfmann.mosby.mvp.rx.scheduler.AndroidSchedulerTransformer;
+
 import rx.Observable;
 import rx.Subscriber;
 
@@ -33,22 +33,10 @@ import rx.Subscriber;
  * @author Hannes Dorfmann
  * @since 1.0.0
  */
-public abstract class MvpLceRxPresenter<V extends MvpLceView<M>, M>
-    extends com.hannesdorfmann.mosby.mvp.MvpBasePresenter<V>
-    implements com.hannesdorfmann.mosby.mvp.MvpPresenter<V> {
+public abstract class MvpLceRxPresenter<V extends MvpLceView<M>, M> extends MvpRxPresenter<V, M> {
 
   protected Subscriber<M> subscriber;
-
-  /**
-   * Unsubscribes the subscriber and set it to null
-   */
-  protected void unsubscribe() {
-    if (subscriber != null && !subscriber.isUnsubscribed()) {
-      subscriber.unsubscribe();
-    }
-
-    subscriber = null;
-  }
+  protected boolean pullToRefresh;
 
   /**
    * Subscribes the presenter himself as subscriber on the observable
@@ -57,67 +45,34 @@ public abstract class MvpLceRxPresenter<V extends MvpLceView<M>, M>
    * @param pullToRefresh Pull to refresh?
    */
   public void subscribe(Observable<M> observable, final boolean pullToRefresh) {
+    this.pullToRefresh = pullToRefresh;
+  }
 
+  @Override
+  public void onRxSubscribe() {
     if (isViewAttached()) {
       getView().showLoading(pullToRefresh);
     }
-
-    unsubscribe();
-
-    subscriber = new Subscriber<M>() {
-      private boolean ptr = pullToRefresh;
-
-      @Override public void onCompleted() {
-        MvpLceRxPresenter.this.onCompleted();
-      }
-
-      @Override public void onError(Throwable e) {
-        MvpLceRxPresenter.this.onError(e, ptr);
-      }
-
-      @Override public void onNext(M m) {
-        MvpLceRxPresenter.this.onNext(m);
-      }
-    };
-
-    observable = applyScheduler(observable);
-    observable.subscribe(subscriber);
   }
 
-  /**
-   * Called in {@link #subscribe(Observable, boolean)} to set  <code>subscribeOn()</code> and
-   * <code>observeOn()</code>. As default it uses {@link AndroidSchedulerTransformer}. Override
-   * this
-   * method if you want to provide your own scheduling implementation.
-   */
-  protected Observable<M> applyScheduler(Observable<M> observable) {
-    return observable.compose(new AndroidSchedulerTransformer<M>());
-  }
-
-  protected void onCompleted() {
-    if (isViewAttached()) {
-      getView().showContent();
-    }
-    unsubscribe();
-  }
-
-  protected void onError(Throwable e, boolean pullToRefresh) {
+  @Override
+  public void onRxError(Throwable e) {
     if (isViewAttached()) {
       getView().showError(e, pullToRefresh);
     }
-    unsubscribe();
   }
 
-  protected void onNext(M data) {
+  @Override
+  public void onRxNext(M data) {
     if (isViewAttached()) {
       getView().setData(data);
     }
   }
 
-  @Override public void detachView(boolean retainInstance) {
-    super.detachView(retainInstance);
-    if (!retainInstance) {
-      unsubscribe();
+  @Override
+  public void onRxCompleted() {
+    if (isViewAttached()) {
+      getView().showContent();
     }
   }
 }
--- rx/src/main/java/com/hannesdorfmann/mosby/mvp/rx/MvpRxPresenter.java
+++ rx/src/main/java/com/hannesdorfmann/mosby/mvp/rx/MvpRxPresenter.java
@@ -1,7 +1,7 @@
 package com.hannesdorfmann.mosby.mvp.rx;
 
 
-import com.hannesdorfmann.mosby.mvp.lce.MvpLceView;
+import com.hannesdorfmann.mosby.mvp.MvpView;
 import com.hannesdorfmann.mosby.mvp.rx.scheduler.AndroidSchedulerTransformer;
 
 import rx.Observable;
@@ -10,7 +10,7 @@ import rx.Subscriber;
 /**
  * A presenter for RxJava, that assumes that only one Observable is subscribed by this presenter.
  * The idea is, that you make your (chain of) Observable and pass it to {@link
- * #subscribe(Observable, boolean)}. The presenter internally subscribes himself as Subscriber to the
+ * #subscribe(Observable)}. The presenter internally subscribes himself as Subscriber to the
  * observable
  * (which executes the observable). Before subscribing the presenter calls
  * {@link #applyScheduler(Observable)} to apply the <code>subscribeOn()</code> and
@@ -19,7 +19,7 @@ import rx.Subscriber;
  * @author Fahim Karim
  * @since 1.0.0
  */
-public abstract class MvpRxPresenter<V extends MvpLceView<M>, M>
+public abstract class MvpRxPresenter<V extends MvpView, M>
         extends com.hannesdorfmann.mosby.mvp.MvpBasePresenter<V>
         implements com.hannesdorfmann.mosby.mvp.MvpPresenter<V> {
 
@@ -40,29 +40,24 @@ public abstract class MvpRxPresenter<V extends MvpLceView<M>, M>
      * Subscribes the presenter himself as subscriber on the observable
      *
      * @param observable The observable to subscribe
-     * @param pullToRefresh Pull to refresh?
      */
-    public void subscribe(Observable<M> observable, final boolean pullToRefresh) {
+    public void subscribe(Observable<M> observable) {
 
-        if (isViewAttached()) {
-            getView().showLoading(pullToRefresh);
-        }
+        onRxSubscribe();
 
         unsubscribe();
 
         subscriber = new Subscriber<M>() {
-            private boolean ptr = pullToRefresh;
-
             @Override public void onCompleted() {
-                MvpLceRxPresenter.this.onCompleted();
+                MvpRxPresenter.this.onCompleted();
             }
 
             @Override public void onError(Throwable e) {
-                MvpLceRxPresenter.this.onError(e, ptr);
+                MvpRxPresenter.this.onError(e);
             }
 
             @Override public void onNext(M m) {
-                MvpLceRxPresenter.this.onNext(m);
+                MvpRxPresenter.this.onNext(m);
             }
         };
 
@@ -71,7 +66,7 @@ public abstract class MvpRxPresenter<V extends MvpLceView<M>, M>
     }
 
     /**
-     * Called in {@link #subscribe(Observable, boolean)} to set  <code>subscribeOn()</code> and
+     * Called in {@link #subscribe(Observable)} to set  <code>subscribeOn()</code> and
      * <code>observeOn()</code>. As default it uses {@link AndroidSchedulerTransformer}. Override
      * this
      * method if you want to provide your own scheduling implementation.
@@ -81,24 +76,23 @@ public abstract class MvpRxPresenter<V extends MvpLceView<M>, M>
     }
 
     protected void onCompleted() {
-        if (isViewAttached()) {
-            getView().showContent();
-        }
+        onRxCompleted();
         unsubscribe();
     }
 
-    protected void onError(Throwable e, boolean pullToRefresh) {
-        if (isViewAttached()) {
-            getView().showError(e, pullToRefresh);
-        }
+    protected void onError(Throwable e) {
+        onRxError(e);
         unsubscribe();
     }
 
     protected void onNext(M data) {
-        if (isViewAttached()) {
-            getView().setData(data);
-        }
+        onRxNext(data);
     }
+
+    public abstract void onRxSubscribe();
+    public abstract void onRxNext(M data);
+    public abstract void onRxError(Throwable e);
+    public abstract void onRxCompleted();
 
     @Override public void detachView(boolean retainInstance) {
         super.detachView(retainInstance);

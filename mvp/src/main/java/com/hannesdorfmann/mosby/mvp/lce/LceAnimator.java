51d547cb305a678eb79190356b93664068638740
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2015-08-26 21:59:42 +0200

    #76 remove SDK < 14 check since minSDK is 14

--- mvp/src/main/java/com/hannesdorfmann/mosby/mvp/lce/LceAnimator.java
+++ mvp/src/main/java/com/hannesdorfmann/mosby/mvp/lce/LceAnimator.java
@@ -20,9 +20,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -40,7 +38,8 @@ public class LceAnimator {
    * Show the loading view. No animations, because sometimes loading things is pretty fast (i.e.
    * retrieve data from memory cache).
    */
-  public static void showLoading(@NonNull View loadingView, @NonNull View contentView, @NonNull View errorView) {
+  public static void showLoading(@NonNull View loadingView, @NonNull View contentView,
+      @NonNull View errorView) {
 
     contentView.setVisibility(View.GONE);
     errorView.setVisibility(View.GONE);
@@ -53,47 +52,36 @@ public class LceAnimator {
   public static void showErrorView(@NonNull final View loadingView, @NonNull final View contentView,
       final TextView errorView) {
 
-    if (Build.VERSION.SDK_INT < 11) {
-      // Before honeycomb
-      contentView.setVisibility(View.GONE);
-      loadingView.setVisibility(View.GONE);
-      errorView.setVisibility(View.VISIBLE);
-    } else {
+    contentView.setVisibility(View.GONE);
 
-      contentView.setVisibility(View.GONE);
+    // Not visible yet, so animate the view in
+    AnimatorSet set = new AnimatorSet();
+    ObjectAnimator in = ObjectAnimator.ofFloat(errorView, "alpha", 1f);
+    ObjectAnimator loadingOut = ObjectAnimator.ofFloat(loadingView, "alpha", 0f);
 
-      // Not visible yet, so animate the view in
-      AnimatorSet set = new AnimatorSet();
-      ObjectAnimator in = ObjectAnimator.ofFloat(errorView, "alpha", 1f);
-      ObjectAnimator loadingOut = ObjectAnimator.ofFloat(loadingView, "alpha", 0f);
+    set.playTogether(in, loadingOut);
+    set.setDuration(200);
 
-      set.playTogether(in, loadingOut);
-      set.setDuration(200);
+    set.addListener(new AnimatorListenerAdapter() {
 
-      set.addListener(new AnimatorListenerAdapter() {
+      @Override public void onAnimationStart(Animator animation) {
+        super.onAnimationStart(animation);
+        errorView.setVisibility(View.VISIBLE);
+      }
 
-        @Override
-        public void onAnimationStart(Animator animation) {
-          super.onAnimationStart(animation);
-          errorView.setVisibility(View.VISIBLE);
-        }
+      @Override public void onAnimationEnd(Animator animation) {
+        super.onAnimationEnd(animation);
+        loadingView.setVisibility(View.GONE);
+        loadingView.setAlpha(1f); // For future showLoading calls
+      }
+    });
 
-        @Override
-        public void onAnimationEnd(Animator animation) {
-          super.onAnimationEnd(animation);
-          loadingView.setVisibility(View.GONE);
-          loadingView.setAlpha(1f); // For future showLoading calls
-        }
-      });
-
-      set.start();
-    }
+    set.start();
   }
 
   /**
    * Display the content instead of the loadingView
    */
-  @TargetApi(11)
   public static void showContent(@NonNull final View loadingView, @NonNull final View contentView,
       @NonNull final View errorView) {
 
@@ -103,52 +91,39 @@ public class LceAnimator {
       loadingView.setVisibility(View.GONE);
     } else {
 
-      if (Build.VERSION.SDK_INT < 14) {
-        // Before honeycomb
+      errorView.setVisibility(View.GONE);
 
-        loadingView.setVisibility(View.GONE);
-        errorView.setVisibility(View.GONE);
-        contentView.setVisibility(View.VISIBLE);
-      } else {
+      int translateDp = 40;
+      // Not visible yet, so animate the view in
+      AnimatorSet set = new AnimatorSet();
+      ObjectAnimator contentFadeIn = ObjectAnimator.ofFloat(contentView, "alpha", 0f, 1f);
+      ObjectAnimator contentTranslateIn = ObjectAnimator.ofFloat(contentView, "translationY",
+          dpToPx(loadingView.getContext(), translateDp), 0);
 
-        // ICS and above
+      ObjectAnimator loadingFadeOut = ObjectAnimator.ofFloat(loadingView, "alpha", 1f, 0f);
+      ObjectAnimator loadingTranslateOut = ObjectAnimator.ofFloat(loadingView, "translationY", 0,
+          -dpToPx(loadingView.getContext(), translateDp));
 
-        errorView.setVisibility(View.GONE);
+      set.playTogether(contentFadeIn, contentTranslateIn, loadingFadeOut, loadingTranslateOut);
+      set.setDuration(500);
 
-        int translateDp = 40;
-        // Not visible yet, so animate the view in
-        AnimatorSet set = new AnimatorSet();
-        ObjectAnimator contentFadeIn = ObjectAnimator.ofFloat(contentView, "alpha", 0f, 1f);
-        ObjectAnimator contentTranslateIn = ObjectAnimator.ofFloat(contentView, "translationY",
-            dpToPx(loadingView.getContext(), translateDp), 0);
+      set.addListener(new AnimatorListenerAdapter() {
 
-        ObjectAnimator loadingFadeOut = ObjectAnimator.ofFloat(loadingView, "alpha", 1f, 0f);
-        ObjectAnimator loadingTranslateOut = ObjectAnimator.ofFloat(loadingView, "translationY", 0,
-            -dpToPx(loadingView.getContext(), translateDp));
+        @Override public void onAnimationStart(Animator animation) {
+          contentView.setTranslationY(0);
+          loadingView.setTranslationY(0);
+          contentView.setVisibility(View.VISIBLE);
+        }
 
-        set.playTogether(contentFadeIn, contentTranslateIn, loadingFadeOut, loadingTranslateOut);
-        set.setDuration(500);
+        @Override public void onAnimationEnd(Animator animation) {
+          loadingView.setVisibility(View.GONE);
+          loadingView.setAlpha(1f); // For future showLoading calls
+          contentView.setTranslationY(0);
+          loadingView.setTranslationY(0);
+        }
+      });
 
-        set.addListener(new AnimatorListenerAdapter() {
-
-          @Override
-          public void onAnimationStart(Animator animation) {
-            contentView.setTranslationY(0);
-            loadingView.setTranslationY(0);
-            contentView.setVisibility(View.VISIBLE);
-          }
-
-          @Override
-          public void onAnimationEnd(Animator animation) {
-            loadingView.setVisibility(View.GONE);
-            loadingView.setAlpha(1f); // For future showLoading calls
-            contentView.setTranslationY(0);
-            loadingView.setTranslationY(0);
-          }
-        });
-
-        set.start();
-      }
+      set.start();
     }
   }
 

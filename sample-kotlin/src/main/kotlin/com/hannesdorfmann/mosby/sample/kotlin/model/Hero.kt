231df0b004016a330d53cfd8434f45792b008d19
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2016-12-04 22:59:10 +0100

    Removed dagger examples

--- sample-kotlin/src/main/kotlin/com/hannesdorfmann/mosby/sample/kotlin/model/Hero.kt
+++
@@ -1,8 +0,0 @@
-package com.hannesdorfmann.mosby.sample.kotlin.model
-
-/**
- * Simple POJO to hold data of a hero
- *
- * @author Hannes Dorfmann
- */
-data class Hero(val name: String, val imageUrl: String)

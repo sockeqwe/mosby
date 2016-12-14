f878d7cdd0b3610b56c1495fe189c761c0ec1dfa
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2016-12-14 10:56:55 +0100

    Added tests for utils-fragment

---
+++ utils-fragment-integration-test/src/main/java/com/hannesdorfmann/mosby3/utils/fragment/integrationtest/backstack/ChildChildFragmentOnBackstack.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 Hannes Dorfmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.hannesdorfmann.mosby3.utils.fragment.integrationtest.backstack;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.BackstackAccessor;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.hannesdorfmann.mosby3.utils.fragment.integrationtest.R;
+
+/**
+ * @author Hannes Dorfmann
+ */
+
+public class ChildChildFragmentOnBackstack extends Fragment {
+
+  public static boolean onBackStack = false;
+
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+
+    View view = inflater.inflate(R.layout.fragment_child_child, container, false);
+    TextView tv = (TextView)view.findViewById(R.id.text);
+    tv.setText(getClass().getSimpleName());
+
+    return view;
+  }
+
+  @Override public void onStop() {
+    super.onStop();
+    onBackStack  = BackstackAccessor.isFragmentOnBackStack(this);
+  }
+}

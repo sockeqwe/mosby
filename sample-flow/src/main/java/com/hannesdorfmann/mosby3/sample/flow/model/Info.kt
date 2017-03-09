8cd6da24627b8cd7bd4f9f2a01c54871c44e058d
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-03-09 11:03:02 +0100

    Removed sample-flow and kotlin dependencies

--- sample-flow/src/main/java/com/hannesdorfmann/mosby3/sample/flow/model/Info.kt
+++
@@ -1,8 +0,0 @@
-package com.hannesdorfmann.mosby3.sample.flow.model
-
-/**
- *
- *
- * @author Hannes Dorfmann
- */
-interface Info
--- sample-flow/src/main/java/com/hannesdorfmann/mosby3/sample/flow/model/InfoPicture.kt
+++
@@ -1,8 +0,0 @@
-package com.hannesdorfmann.mosby3.sample.flow.model
-
-/**
- *
- *
- * @author Hannes Dorfmann
- */
-data class InfoPicture(val url: String) : Info

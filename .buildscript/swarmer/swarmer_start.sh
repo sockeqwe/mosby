5ebe58ed1712763ca09b1433383ccdecffd783a6
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-07-12 11:47:41 +0200

    Fixing circle ci emulator sh

--- .buildscript/swarmer/swarmer_start.sh
+++ .buildscript/swarmer/swarmer_start.sh
@@ -11,7 +11,7 @@ sdkmanager "system-images;android-25;google_apis;x86_64"
 java -jar ${SWARMER_JAR} start \
 --emulator-name test_emulator_1 \
 --package "system-images;android-25;google_apis;x86_64" \
---android-abi google_apis/x86 \
+--android-abi google_apis/x86_64 \
 --path-to-config-ini emulator1-config.ini \
 --emulator-start-options -prop persist.sys.language=en -prop persist.sys.country=US \
 --redirect-logcat-to test_emulator_1_logcat.txt

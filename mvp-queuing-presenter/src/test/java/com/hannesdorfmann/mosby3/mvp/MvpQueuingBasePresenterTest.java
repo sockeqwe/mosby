20c46cc9d5af0c98c77223b8ce72b20831b6a2ed
James Shvarts <jshvarts@gmail.com>
2017-12-17 08:32:13 -0500

    fix typos in README (#295)

--- mvp-queuing-presenter/src/test/java/com/hannesdorfmann/mosby3/mvp/MvpQueuingBasePresenterTest.java
+++ mvp-queuing-presenter/src/test/java/com/hannesdorfmann/mosby3/mvp/MvpQueuingBasePresenterTest.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MvpQueueingBasePresenterTest {
+public class MvpQueuingBasePresenterTest {
   @Test public void queueActionsAndExecuteOnceViewAttached() throws Exception {
 
     TestView view = new TestView();

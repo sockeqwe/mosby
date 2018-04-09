2b0e7648ec0da39c05d020718e7bd59d65ce8296
sockeqwe <hannes.dorfmann@gmail.com>
2018-04-09 19:19:41 +0200

    ActivityMviDelegateImpl now checks if presenter != null if Activity.finish() is called in Activity.onCreate()  #290

---
+++ mvi-integration-test/src/main/java/com/hannesdorfmann/mosby3/mvi/integrationtest/lifecycle/activity/MviFinishInOnStartActivity.java
@@ -0,0 +1,36 @@
+package com.hannesdorfmann.mosby3.mvi.integrationtest.lifecycle.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.hannesdorfmann.mosby3.mvi.MviActivity;
+import com.hannesdorfmann.mosby3.mvi.integrationtest.lifecycle.LifecycleTestPresenter;
+import com.hannesdorfmann.mosby3.mvi.integrationtest.lifecycle.LifecycleTestView;
+
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.Subject;
+
+public class MviFinishInOnStartActivity extends MviActivity<LifecycleTestView, LifecycleTestPresenter> implements LifecycleTestView {
+
+    @NonNull
+    @Override
+    public LifecycleTestPresenter createPresenter() {
+        return new LifecycleTestPresenter();
+    }
+
+    public Subject<Boolean> onDestroyReached  = BehaviorSubject.create();
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        finish(); // finish  --> this is what we would like to test --> App should not crash
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        onDestroyReached.onNext(true);
+        onDestroyReached.onComplete();
+    }
+}

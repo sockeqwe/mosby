87df43261cb68da139ef5a576687301ffc3f1759
azEf <azEf@users.noreply.github.com>
2018-12-03 15:03:17 +0100

    fix: MviDialogFragment extends DialogFragment (#325)

--- mvi/src/main/java/com/hannesdorfmann/mosby3/mvi/MviDialogFragment.java
+++ mvi/src/main/java/com/hannesdorfmann/mosby3/mvi/MviDialogFragment.java
@@ -24,6 +24,7 @@ import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -55,7 +56,7 @@ import com.hannesdorfmann.mosby3.mvp.MvpView;
  * @author Hannes Dorfmann
  * @since 3.0.0
  */
-public abstract class MviDialogFragment<V extends MvpView, P extends MviPresenter<V, ?>> extends Fragment
+public abstract class MviDialogFragment<V extends MvpView, P extends MviPresenter<V, ?>> extends DialogFragment
     implements MvpView, MviDelegateCallback<V, P> {
 
   private boolean isRestoringViewState = false;
